1fa36b3f798f8affb0a45db63668e29247b1efc5
Diane <hello@dianenguyen.ca>
2023-12-18 14:45:43 -0800

    Separated steam auth and game analyzing into two different API's. Setting up front end for app

---
+++ steam-ai-chatbot-ui/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Steam AI Library Analyzer',
+  description: 'Chat GPT 4 & Steam Web API',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
